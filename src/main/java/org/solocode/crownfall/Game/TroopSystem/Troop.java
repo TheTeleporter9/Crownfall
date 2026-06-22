56d71af68af7a9f5ddcbbdbc91d2fa388e673895
TheTeleporter9 <the.code.load1@gmial.com>
2026-06-22 15:38:12 +0200

    MORE PROGRESS

--- src/main/java/org/solocode/crownfall/Game/TroopSystem/Troop.java
+++ src/main/java/org/solocode/crownfall/Game/TroopSystem/Troop.java
@@ -1,10 +1,15 @@
 package org.solocode.crownfall.Game.TroopSystem;
 
+import com.destroystokyo.paper.entity.Pathfinder;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
 import org.bukkit.World;
 import org.bukkit.entity.*;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import org.solocode.crownfall.Crownfall;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,9 +20,11 @@ public class Troop {
 
     private List<TroopMember> troopMembers = new ArrayList<>();
     private int troopSize = 1;
+    private NamespacedKey namespacedKey;
 
-    public Troop(int troopSize, TroopTypes troopTypes, Player troopOwner) {
+    public Troop(int troopSize, TroopTypes troopTypes, Player troopOwner, NamespacedKey namespacedKey) {
         this.troopSize = troopSize;
+        this.namespacedKey = namespacedKey;
         troopMembers.add(
                 new TroopMember(troopTypes, 1, troopOwner)
         );
@@ -58,8 +65,15 @@ public class Troop {
 
             mob.setCustomNameVisible(true);
 
+            mob.setAI(false);
 
+            mob.addScoreboardTag("troop");
+            mob.addScoreboardTag(troopMember.getTroopType().toString() + "-troop");
 
+            //Set the mob/entity selected to false
+            mob.getPersistentDataContainer().set(namespacedKey, PersistentDataType.BOOLEAN, false);
+
+            troopMember.setMob(mob);
         }
 
     }
@@ -76,7 +90,29 @@ public class Troop {
         return null;
     }
 
+    /***
+     * Make troop entities go to a location
+     * Only if they have the data tag, of selected
+     */
+    public void goTo(Location location) {
+        World world = location.getWorld();
 
+        for(TroopMember troopMember : troopMembers) {
+            Mob troopMemberMob = troopMember.getMob();
+            if(!(troopMemberMob.getScoreboardTags().contains("troop"))) return;
+
+            PersistentDataContainer pdc = troopMemberMob.getPersistentDataContainer();
+            Boolean selected = pdc.get(namespacedKey, PersistentDataType.BOOLEAN);
+
+            //Make shure that it is the correct troop;
+            assert selected != null;
+            if(!selected) return;
+
+            Pathfinder pathfinder = troopMemberMob.getPathfinder();
+            pathfinder.moveTo(location);
+
+        }
+    }
 
 
     // This class represents one unit of a troop!
@@ -86,6 +122,7 @@ public class Troop {
         private int troopMemberLevel = 0;
         private Player owner;
         private boolean alive = true;
+        private Mob mob;
 
         public TroopMember(TroopTypes type, int troopMemberLevel, Player troopMemberOwner) {
             this.troopType = type;
@@ -97,6 +134,13 @@ public class Troop {
             return troopType;
         }
 
+        public Mob setMob(Mob mob) {
+            return this.mob = mob;
+        }
+
+        public Mob getMob() {
+            return mob;
+        }
 
         public int getTroopMemberLevel() {
             return troopMemberLevel;
--- src/main/java/org/solocode/crownfall/Game/TroopSystem/TroopManager.java
+++ src/main/java/org/solocode/crownfall/Game/TroopSystem/TroopManager.java
@@ -1,4 +1,18 @@
 package org.solocode.crownfall.Game.TroopSystem;
 
+import org.bukkit.NamespacedKey;
+import org.bukkit.plugin.Plugin;
+import org.solocode.crownfall.Crownfall;
+
 public class TroopManager {
+    private Plugin plugin;
+    private final NamespacedKey TROOP_SELECTOR_KEY;
+
+    public TroopManager(Crownfall plugin) {
+        this.plugin = plugin;
+
+        TROOP_SELECTOR_KEY = new NamespacedKey(plugin, "selected");
+    }
+
+
 }
